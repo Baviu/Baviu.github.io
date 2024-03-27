7078b8b871688231f6c917d4d23232c5523baaae
Baviu <163061890+Baviu@users.noreply.github.com>
2024-03-27 22:10:27 +0200

    2121

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ function toggleMenu() {
   var menu = document.getElementById("menu");
   if (menu.style.display === "none") {
     menu.style.display = "block";
-  } else {
+  } else {const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+  const menuItems = document.querySelectorAll(".menu-items");
+  menuItems.forEach((menuItem) => {
+    if (menuItem.style.display === "block") {
+      menuItem.style.display = "none";
+      return;
+    }
+    // Check if the submenu is open and close it
+    const submenu = menuItem.querySelector(".submenu");
+    if (submenu && submenu.style.display === "block") {
+      submenu.style.display = "none";
+      return;
+    }
+  });
+});
     menu.style.display = "none";
   }
 }
